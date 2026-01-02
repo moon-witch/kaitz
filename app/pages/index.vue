629047286b472f0b6cba6f676486046ac679a302
moonwitch <joshua_develops@proton.me>
2026-01-02 13:10:57 +0100

    replaced vue with nuxt

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {KaitzLogo} from "#components";
+</script>
+
+<template>
+  <main>
+    <KaitzLogo/>
+
+    <h1>
+      Hallooooo, ich bin Kaitz'!
+    </h1>
+  </main>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+    css: ['~/assets/styles/main.scss'],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "~/assets/styles/_variables.scss" as *;',
+                },
+            },
+        },
+    },
+})
